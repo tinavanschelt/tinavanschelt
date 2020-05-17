062d11059f70afeb4cb1330910e696fc8f4e8c14
Tina <tinavanschelt@gmail.com>
2020-05-17 13:03:30 +0100

    fix page title

--- src/components/PageTitle.vue
+++ src/components/PageTitle.vue
@@ -11,9 +11,15 @@ export default {
 <style lang="scss">
 .page-title {
   color: var(--page-title-color);
-  font-size: 6rem;
+  font-size: 5rem;
   font-weight: 600;
-  margin-bottom: -3.35rem;
-  padding: 0 2rem;
+  margin-bottom: -2.75rem;
+  padding: 0 1rem;
+
+  @media screen and (min-width: 600px) {
+    font-size: 6rem;
+    margin-bottom: -3.35rem;
+    padding: 0 2rem;
+  }
 }
 </style>
